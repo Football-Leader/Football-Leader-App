4fde7805ec456ed7b85494ddcda151751bad50df
Michael Lelyakin <michael.lelyakin@benchmedia.com>
2020-12-20 14:37:00 +0400

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # остановить публикацию при ошибках
-set -e
+# set -e
 
 # сборка
 npm run build
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:Imperat/Imperat.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:Football-Leader/Football-Leader-App.git master:gh-pages
 
 cd -
